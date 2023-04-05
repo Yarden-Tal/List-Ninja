3db1e0f16c1cc6c5b00eb474de11249218e7def9
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-04-05 15:25:44 +0300

    made vercel adjustments

--- public/api.js
+++ public/api.js
@@ -2,25 +2,25 @@
 
 // GET
 async function getAllTasks() {
-    const tasksData = await axios.get('/tasks')
-    return tasksData.data
+  console.log(123);
+  const tasksData = await axios.get("/tasks");
+  return tasksData.data;
 }
 // POST
 async function postTask(text) {
-    const res = await axios.post('/tasks', { text })
-    console.log(res);
-    return res.data
+  const res = await axios.post("/tasks", { text });
+  console.log(res);
+  return res.data;
 }
 // PUT
 async function putTask(task) {
-    const res = await axios.put('/tasks', { task })
-    console.log(res);
-    return res.data
+  const res = await axios.put("/tasks", { task });
+  console.log(res);
+  return res.data;
 }
 // DELETE
 async function deleteTask(taskId) {
-    const res = await axios.delete(`/tasks/${taskId}`)
-    console.log(res);
-    return res.data
-
+  const res = await axios.delete(`/tasks/${taskId}`);
+  console.log(res);
+  return res.data;
 }
--- public/app.js
+++ public/app.js
@@ -11,104 +11,112 @@ Functions:
 */
 
 // The tasks array
-const tasks = []
+const tasks = [];
 
 /* Add */
-const form = document.querySelector('#add-task-form')
-const modal = document.querySelector('.add-task-modal')
-form.addEventListener('submit', handleSubmit)
+const form = document.querySelector("#add-task-form");
+const modal = document.querySelector(".add-task-modal");
+form.addEventListener("submit", handleSubmit);
 
 async function handleSubmit(ev) {
-    ev.preventDefault()
-    const taskText = ev.target[0].value
-    if (!taskText) throw new Error('Please enter description.')
-    const newTask = await postTask(taskText)
-    tasks.push(newTask)
-    renderTaskList()
-    modal.classList.add('hide')
-    ev.target.reset()
+  ev.preventDefault();
+  const taskText = ev.target[0].value;
+  if (!taskText) throw new Error("Please enter description.");
+  const newTask = await postTask(taskText);
+  tasks.push(newTask);
+  renderTaskList();
+  modal.classList.add("hide");
+  ev.target.reset();
 }
 
 /* Render */
 const renderTaskList = () => {
-    // Check if task is done, to add the class "done"
-    function checkIsDone(isDone) {
-        if (isDone) {
-            return "done"
-        }
+  // Check if task is done, to add the class "done"
+  function checkIsDone(isDone) {
+    if (isDone) {
+      return "done";
     }
-    let taskHtml = tasks.map(task => {
-    return (`
+  }
+  let taskHtml = tasks
+    .map(task => {
+      return `
 <div class="task-wrapper">
     <div class="task">  
         <div class="task__done-button">
-            <img src="./images/sword.png" alt="Done" onclick='toggleDone("${task.id}")'>
+            <img src="./images/sword.png" alt="Done" onclick='toggleDone("${
+              task.id
+            }")'>
         </div>
         <p class="task__text ${checkIsDone(task.isDone)}">${task.text}</p>
         <div class="task__buttons-wrapper">
             <div class="edit-button-wrapper">
-                <i onclick='getIdForUpdate("${task.id}")' class="fas fa-pencil-alt fa-lg"></i>
+                <i onclick='getIdForUpdate("${
+                  task.id
+                }")' class="fas fa-pencil-alt fa-lg"></i>
             </div>
             <div class="delete-button-wrapper">
-                <i onclick='handleDelete("${task.id}")' class="fas fa-trash fa-lg"></i>
+                <i onclick='handleDelete("${
+                  task.id
+                }")' class="fas fa-trash fa-lg"></i>
             </div>
         </div>
     </div>
 </div> 
-`)
-    }).join('')
-    document.querySelector('.list__body').innerHTML = taskHtml
-}
+`;
+    })
+    .join("");
+  document.querySelector(".list__body").innerHTML = taskHtml;
+};
 
 /* Update */
 function getIdForUpdate(taskId) {
-    const id = document.querySelector('#task-id')
-    id.value = taskId
-    openEditModal()
-    const taskToUpdate = tasks.find(task => task.id === taskId)
-    return taskToUpdate.id
+  const id = document.querySelector("#task-id");
+  id.value = taskId;
+  openEditModal();
+  const taskToUpdate = tasks.find(task => task.id === taskId);
+  return taskToUpdate.id;
 }
 
 async function handleUpdate(ev) {
-    ev.preventDefault()
-    const taskId = document.querySelector('#task-id').value
-    const taskIndex= tasks.findIndex(task => task.id === taskId)
-    const taskToUpdate = tasks[taskIndex]
-    taskToUpdate.text = ev.target.elements[0].value
-    console.log(taskToUpdate)
-    const updatedTask = await putTask(taskToUpdate)
-    tasks[taskIndex] = updatedTask
-    ev.target.reset()
-    closeEditModal()
-    renderTaskList()
+  ev.preventDefault();
+  const taskId = document.querySelector("#task-id").value;
+  const taskIndex = tasks.findIndex(task => task.id === taskId);
+  const taskToUpdate = tasks[taskIndex];
+  taskToUpdate.text = ev.target.elements[0].value;
+  console.log(taskToUpdate);
+  const updatedTask = await putTask(taskToUpdate);
+  tasks[taskIndex] = updatedTask;
+  ev.target.reset();
+  closeEditModal();
+  renderTaskList();
 }
 
 /* Delete */
 async function handleDelete(taskId) {
-    const taskToDelete = tasks.find(task => task.id === taskId)
-    console.dir(taskToDelete);
-    const tasksAfterDelete = await deleteTask(taskToDelete.id)
-    tasks.splice(0, tasks.length)
-    tasks.push(...tasksAfterDelete)
-    console.log(tasks)
-    renderTaskList()
+  const taskToDelete = tasks.find(task => task.id === taskId);
+  console.dir(taskToDelete);
+  const tasksAfterDelete = await deleteTask(taskToDelete.id);
+  tasks.splice(0, tasks.length);
+  tasks.push(...tasksAfterDelete);
+  console.log(tasks);
+  renderTaskList();
 }
 
 // Handle done:
 async function toggleDone(taskId) {
-    const taskIndex = tasks.findIndex(task => task.id === taskId)
-    const task = tasks[taskIndex]
-    task.isDone = !task.isDone
-    const updatedTask = await putTask(task)
-    tasks[taskIndex] = updatedTask
-    renderTaskList()
+  const taskIndex = tasks.findIndex(task => task.id === taskId);
+  const task = tasks[taskIndex];
+  task.isDone = !task.isDone;
+  const updatedTask = await putTask(task);
+  tasks[taskIndex] = updatedTask;
+  renderTaskList();
 }
 
 // Initiation func for getting tasks and rendering
 async function init() {
-    const arr = await getAllTasks()
-    tasks.push(...arr)
-    renderTaskList()
-    console.log(tasks);
+  const arr = await getAllTasks();
+  tasks.push(...arr);
+  renderTaskList();
+  console.log(tasks);
 }
-init()
+init();
